3a7ea7653e51249143ec12f348f325f7576f8115
Adam Gierscher <a.gierscher@live.ca>
2014-09-20 12:49:21 -0400

    User section on gameroom
    Updated the user section on gameroom such that it now displays temp data
    and designs

--- mafia/static/js/players.js
+++ mafia/static/js/players.js
@@ -4,5 +4,5 @@ function initPlayerList()
 
 function resizePlayerList()
 {
-        document.getElementById("player-list").style.height = String(window.innerHeight - 50) + "px";
+    document.getElementById("player-list").style.height = String(window.innerHeight - 50) + "px";
 }
--- mafia/static/js/user.js
+++ mafia/static/js/user.js
@@ -6,4 +6,5 @@ function initUser()
 function resizeUser()
 {
     document.getElementById("user").style.height = String(window.innerHeight - 50) + "px";
+    document.getElementById("user").getElementsByTagName("div")[1].style.height = String(document.getElementById("user").offsetHeight - document.getElementById("user").getElementsByTagName("div")[0].offsetHeight - document.getElementById("user").getElementsByTagName("div")[2].offsetHeight - 20) + "px";
 }
