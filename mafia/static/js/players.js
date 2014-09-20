228dd768dd0a8e43fd1220082969fa33f939e794
Adam Gierscher <a.gierscher@live.ca>
2014-09-20 16:41:51 -0400

    Saving for the safety of saving work

--- mafia/static/js/players.js
+++ mafia/static/js/players.js
@@ -1,8 +1,20 @@
+var players = [];
+
 function initPlayerList()
 {
+    // Test
+    players[0] = new Player("Adam", 0, Infinity, undefined)
 }
 
 function resizePlayerList()
 {
     document.getElementById("player-list").style.height = String(window.innerHeight - 50) + "px";
+}
+
+function Player(username, lynches, elections, thoughts)
+{
+    this.username = username;
+    this.lynches = Number(lynches);
+    this.elections = Number(elections);
+    this.thoughts = thoughts;
 }
