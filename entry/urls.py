57f41b4da942f42450df76282cea3043b874f8da
DrTeePot <tristanpotter@rogers.com>
2014-09-20 18:35:50 -0400

    fix silly error in forms

--- entry/urls.py
+++ entry/urls.py
@@ -3,6 +3,6 @@ from django.conf.urls import patterns, include, url
 
 urlpatterns = patterns('',
     url(r'^$', 'entry.views.home', name='index'),
-    url(r'^create-user$', 'entry.views.create_user', name='create-user'),
+    url(r'^create-user$', 'entry.views.register', name='register'),
     url(r'^login$', 'entry.views.login', name='login'),
 )
