6daeb3a47eb7b3dea208e39dbaccbffa09304b64
DrTeePot <tristanpotter@rogers.com>
2014-09-20 23:21:36 -0400

    hotfix for function not existing

--- mafia/urls.py
+++ mafia/urls.py
@@ -6,7 +6,7 @@ urlpatterns = patterns('',
                        url(r'create', 'mafia.views.create_room', name='create_room'),
                        url(r'add', 'mafia.views.add_room', name='add_room'),
                        url(r'send_message', 'mafia.views.send_message', name='send_message'),
-                       url(r'get_messages', 'mafia.views.get_messages', name='retrieve_messages'),
+                       url(r'get_messages', 'mafia.views.check_data', name='retrieve_messages'),
                        url(r'game', 'mafia.views.enter_room', name='game_room'),
                        url(r'game/vote', 'mafia.views.vote', name='vote')
 )
